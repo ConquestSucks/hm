424ae43a4c3479a556800759d40794f8cf3e1f85
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2025-07-27 13:36:42 +0500

    init

---
+++ viperr-frontend/src/components/Loading.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+
+    return (
+        <div className="h-full">
+            <img src="/public/loading.gif" alt="" className="w-full h-full" />
+        </div>
+    )
+}
+
+export default Loading
