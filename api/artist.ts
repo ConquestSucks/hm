9fb2ec69f2b133ff5c232bd89c207bd881440a75
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2025-07-27 15:25:32 +0500

    no more monorepo

--- api/artist.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default async function handler(requst: VercelRequest, response: VercelResponse) {
-
-    return response.json({
-        "name": "huesos"
-    })
-}
--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ const Loading = () => {
 
     return (
         <div className="h-full">
-            <img src="/public/loading.gif" alt="" className="w-full h-full" />
+            <img src="src/assets/loading.gif" alt="" className="w-full h-full" />
         </div>
     )
 }
