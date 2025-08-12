74b10abcfafaf413145db6a93c744ec2901a5d43
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2025-08-12 08:00:48 +0500

    misc

--- src/components/music/AlbumCard.tsx
+++ src/components/music/AlbumCard.tsx
@@ -12,7 +12,7 @@ const AlbumCard: React.FC<AlbumCardProps> = ({ item }) => {
             to={item.id}
             className='flex flex-col gap-1 items-center rounded-lg p-4 w-[256px] h-[308px] md:w-[204px] md:h-[256px] cursor-pointer transition-all hover:bg-[#1E1E1E] active:bg-[#1E1E1E]'
         >
-            <img src={item.images[0].url} alt='album image' className='w-[232px] h-[232px] rounded-md md:w-[180px] md:h-[180px] md:rounded-lg' />
+            <img src={item.images[0].url} alt='album image' className='min-w-[232px] min-h-[232px] rounded-md md:min-w-[180px] md:min-h-[180px] md:rounded-lg' />
             <div className='flex flex-col gap-1 w-full'>
                 <span className='text-md font-medium hover:underline'>{item.name}</span>
                 <div className='flex gap-1 text-[#b3b3b3]'>
